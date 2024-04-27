68a8ac05e48d05c5896a334cd8f1185e76490bba
Katrina Lyman <lymankatrina@gmail.com>
2024-04-27 12:34:35 -0600

    Set up html index and login views

--- dist/src/controllers/auth.js
+++ dist/src/controllers/auth.js
@@ -1,9 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = __importDefault(require("path"));
 const checkAuth = (req, res) => {
-    res.send(req.oidc.isAuthenticated()
-        ? `Logged In: Authentication successful. Welcome to the San Juan Theater directory! Add '/api-docs' to the url to view API documentation!`
-        : 'Logged out');
+    if (req.oidc.isAuthenticated()) {
+        res.sendFile(path_1.default.join(__dirname, '../../public/views/loggedIn.html'));
+    }
+    else {
+        res.sendFile(path_1.default.join(__dirname, '../../public/index.html'));
+    }
 };
 const callback = (req, res) => {
     res.send(`Authentication successful. Welcome to the San Juan Theater Directory! Add '/api-docs' to the url to view API documentation!`);
